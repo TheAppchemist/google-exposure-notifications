c77ffbdbf01aef644b10b6f4f55c660b85a31014
Melvin Musehani <melvin@appchemy.co.za>
2021-02-15 13:49:35 +0200

    Added methods to javascript

---
+++ src/android/com/appchemy/ExposureNotificationBroadcastReceiver.java
@@ -0,0 +1,23 @@
+public class ExposureNotificationBroadcastReceiver extends BroadcastReceiver
+{
+  @Override
+  public void onReceive(Context context, Intent intent)
+  {
+    String action = intent.getAction();
+    if (ExposureNotificationClient.ACTION_EXPOSURE_STATE_UPDATED
+        .equals(action))
+      {
+        // Handle exposure found action
+      }
+    else if (ExposureNotificationClient.ACTION_EXPOSURE_NOT_FOUND
+        .equals(action))
+      {
+        // Handle exposure not found action
+      }
+    else if (ExposureNotificationClient.ACTION_SERVICE_STATE_UPDATED
+        .equals(action))
+      {
+        // Handle service state change (for example, user manually disabled it)
+      }
+  }
+}
--- www/google-exposure-notifications.js
+++ www/google-exposure-notifications.js
@@ -1,5 +1,13 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'google-exposure-notifications', 'coolMethod', [arg0]);
+exports.requestPermissions = function (arg0, success, error) {
+    exec(success, error, 'GoogleExposureNotifications', 'requestPermissions', []);
+};
+
+exports.start = function (arg0, success, error) {
+    exec(success, error, 'GoogleExposureNotifications', 'start', [arg0]);
+};
+
+exports.stop = function (arg0, success, error) {
+    exec(success, error, 'GoogleExposureNotifications', 'stop', [arg0]);
 };
